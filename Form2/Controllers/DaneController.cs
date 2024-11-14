73c73c9eb764f7c5c08bf2a2acd12f367ce8f6ab
Ernest Wielguszewski <ernest.wielguszewski@microsoft.wsei.edu.pl>
2024-11-14 23:48:48 +0100

    Dodaj pliki projektów.

---
+++ Form2/Controllers/DaneController.cs
@@ -0,0 +1,54 @@
+﻿using Form2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Form2.Controllers
+{
+    public class DaneController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Form()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Form(Dane dane)
+        {
+            var registeredAccount = new Dane
+            {
+                Imie = dane.Imie,
+                Nazwisko = dane.Nazwisko,
+                Email = dane.Email,
+                Haslo = dane.Haslo,
+                Telefon = dane.Telefon,
+                Wiek = dane.Wiek,
+                Miasto = dane.Miasto
+            };
+
+            // implementacja przekazania danych do bazy danych
+
+            if (ModelState.IsValid)
+            {
+                TempData["SuccessMessage"] = "Rejestracja zakończona pomyślnie!";
+                return View("Wynik", dane);
+            }
+
+            else return View();
+        }
+
+        public ActionResult Wynik(Dane dane)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(dane);
+            }
+
+            return View("Form", dane);
+        }
+    }
+}
---
+++ Form2/Models/Dane.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Form2.Models
+{
+    public class Dane
+    {
+        [Required(ErrorMessage = "Prosze podaj Imie")]
+        [MinLength(2, ErrorMessage = "Imię musi mieć co najmniej 2 znaki")]
+        public string Imie { get; set; }
+
+        [Required(ErrorMessage = "Prosze podaj Nazwisko")]
+        [MinLength(2, ErrorMessage = "Nazwisko musi mieć co najmniej 2 znaki")]
+        public string Nazwisko { get; set; }
+
+        [Required(ErrorMessage = "Prosze podaj Email")]
+        [EmailAddress(ErrorMessage = "Niepoprawny format adresu e-mail")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Prosze podaj Haslo")]
+        [MinLength(8, ErrorMessage = "Hasło musi mieć co najmniej 8 znaków.")]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$", ErrorMessage = "Hasło musi zawierać co najmniej jedną dużą literę, jedną małą literę i jedną cyfrę.")]
+        [DataType(DataType.Password)]
+        public string Haslo { get; set; }
+
+        [Required(ErrorMessage = "Prosze potwierdzic Haslo")]
+        [MinLength(8, ErrorMessage = "Hasło musi mieć co najmniej 8 znaków.")]
+        [Compare("Haslo", ErrorMessage = "Hasla musza sie zgadzac")]
+        [DataType(DataType.Password)]
+        public string PotwierdzHaslo { get; set; }
+
+        [Required(ErrorMessage = "Prosze podaj numer Telefonu")]
+        [RegularExpression(@"^(\+48\s?)?(\d{3}\s?\d{3}\s?\d{3})$",
+            ErrorMessage = "Wprowadz prawidlowy numer Telefonu, np. +48 123 456 789 lub 123 456 789")]
+        public string Telefon { get; set; }
+
+        [Required(ErrorMessage = "Prosze podaj Wiek")]
+        [Range(18, 100, ErrorMessage = "Wiek musi być między 18 a 100")]
+        public int Wiek { get; set; }
+
+        [Required(ErrorMessage = "Prosze wybrac Miasto")]
+        public Miasta Miasto { get; set; }
+
+        public enum Miasta
+        {
+            Krakow,
+            Warszawa,
+            Rzeszow,
+            Wroclaw,
+            Poznan
+        }
+    }
+}
